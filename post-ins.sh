da5f0a108eb941720ea211edbd91de1c14b13ddb
pkill-preston <0v3rthinker@protonmail.com>
2020-12-07 20:51:33 +0000

    fixing the last commit

--- post-ins.sh
+++ post-ins.sh
@@ -55,3 +55,4 @@ grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUBX64
 cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
