cb274c7a3c78b512712c364760c3793d091e671a
pkill-preston <0v3rthinker@protonmail.com>
2020-12-09 06:12:24 -0300

    Script to add some cases

--- esac.sh
+++ esac.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 
 i=1
-
+<<comment
 while [ $i -le 30 ]
 do
 	echo "$i)" >> /home/preston/Documents/code/arch4all/cas.dat
+	let "++i"
+done
+comment
+
+while [ $i -le 15 ] 
+do
+	echo "[ [ '$ZONE' == '$' ] ]" >> ./teste.dat
 	let "++i"
 done
