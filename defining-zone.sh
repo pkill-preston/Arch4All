2115ace907b81189918e97b294026e2a8a6ab242
pkill-preston <0v3rthinker@protonmail.com>
2020-12-09 02:43:20 +0000

    Zone script

---
+++ defining-zone.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+## Defining Region and synching it to the system
+
+
+echo -e "Where zone do you live?
+1)Africa
+2):America
+3)Antarctica
+4)Arctic
+5)Asia
+6)Atlantic
+7)Australia
+8)Brazil
+9)Canada
+10)Chile
+11)Europe
+12)Indian
+13)Mexico
+14)Pacific
+15)US"
+read -p  ": " $ZONE
