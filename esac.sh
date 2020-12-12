54c6d66ce37fd8a291496229cdc5908a33d8c281
pkill-preston <0v3rthinker@protonmail.com>
2020-12-12 09:37:31 +0000

    Updating the case files

--- esac.sh
+++ esac.sh
@@ -1,16 +1,19 @@
 #! /bin/sh
 
 i=1
+
 <<comment
-while [ $i -le 30 ]
+while [ $i -le 14 ]
 do
 	echo "$i)" >> /home/preston/Documents/code/arch4all/cas.dat
 	let "++i"
 done
+
 comment
 
-while [ $i -le 15 ] 
+while [ $i -le 14 ] 
 do
-	echo "[ [ '$ZONE' == '$' ] ]" >> ./teste.dat
-	let "++i"
+	ZONES=(Africa America Antarctica Arctic Asia Atlantic Australia Brazil Canada Chile Europe Indian Mexico Pacific US)
+	echo "[ [ '${ZONES[$i]}' == '$i' ] ]" >> ./cas.dat
+	let "i++"
 done
