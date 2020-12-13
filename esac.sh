963b2b36a1206b74270aad116fe36308e5712e8c
pkill-preston <0v3rthinker@protonmail.com>
2020-12-13 17:00:25 -0300

    case adding script

--- esac.sh
+++ esac.sh
@@ -9,11 +9,18 @@ do
 	let "++i"
 done
 
-comment
+
 
 while [ $i -le 14 ] 
 do
 	ZONES=(Africa America Antarctica Arctic Asia Atlantic Australia Brazil Canada Chile Europe Indian Mexico Pacific US)
 	echo "[ [ '${ZONES[$i]}' == '$i' ] ]" >> ./cas.dat
 	let "i++"
+done
+comment
+
+while [ $i -le 15 ]
+do
+	echo " $i) "INVALID_INPUT";;" >> /home/preston/Documents/code/sh-test/intest.sh
+	let "++i"
 done
